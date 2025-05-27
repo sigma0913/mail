7c3701d032a1d54ce8ecb1720fa7e37ce9943ed2
sigma0913 <sigma0913@outlook.jp>
2025-05-27 17:55:39 +0900

    jingyu added

--- assets/fetch.js
+++ assets/fetch.js
@@ -31,10 +31,10 @@ const tletter = ["ｔ","ʈ","𝘵","𝒕","𝗍","𝐭","𖼹"];
 const largetletter = ["Т","Τ","Ｔ","𐊗","𐊱","𐤯","Ꭲ","𖼊","𑢼","ߠ","ꓔ","𑫝","𐨝","𐝇","𖩋","𐍄"];
 
 
-const username = ["sigma","Tanatana792","Usuharu1189","AKT","生牡蠣","misan"];
-const userpass = ["sigma/17291268","Tanatana792/206","Usuharu1189/1016","AKT/299792458","生牡蠣/1026","misan/2042"];
-const noslashusername = ["AKT","生牡蠣","sigma","Tanatana792","Usuharu1189","misan"]
-const noslashuserpass = ["299792458","1026","17291268","206","1016","2042"]
+const username = ["sigma","Tanatana792","Usuharu1189","AKT","生牡蠣","misan","jingyu"];
+const userpass = ["sigma/17291268","Tanatana792/206","Usuharu1189/1016","AKT/299792458","生牡蠣/1026","misan/2042","jingyu/5173"];
+const noslashusername = ["AKT","生牡蠣","sigma","Tanatana792","Usuharu1189","misan","jingyu"]
+const noslashuserpass = ["299792458","1026","17291268","206","1016","2042","5173"]
 
 let bodytag = document.getElementById("body");
 
